edfd9832a9569bfae2b33c2c9edf8cd2989f871a
AlexRioDe <Make section with stocks like a ticker>
2024-10-31 15:52:03 +0300

    new info tsum

--- app/page.tsx
+++ app/page.tsx
@@ -13,6 +13,7 @@ import {Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, Button} from "@ne
 
 import {useState, useEffect } from "react";
 import {Card, CardBody, CardFooter} from "@nextui-org/card";
+import {CardIcon, TableIcon} from "@/components/icons";
 
 const columns = [
 	{
@@ -49,6 +50,11 @@ const columns = [
 export default function Home() {
 	const [orderList, setOrderList] = useState([]);
 	const [loading, setLoading] = useState(true);
+	const [tableCard, setTableCard] = useState(true);
+	
+	const changeTableCard = () => {
+		setTableCard(!tableCard)
+	}
 	
 	useEffect(() => {
 		(async () => {
@@ -62,72 +68,90 @@ export default function Home() {
 	}, []);
 	return (
 		<section>
-			<Table aria-label="Example table with dynamic content">
-				<TableHeader columns={columns}>
-					{(column) => <TableColumn key={column.key}>{column.label}</TableColumn>}
-				</TableHeader>
-				<TableBody items={orderList}>
-					{(item) => (
-							<TableRow key={item?.id}>
-								<TableCell>{item?.id}</TableCell>
-								<TableCell>{item?.status}</TableCell>
-								<TableCell>{item?.billing?.email}</TableCell>
-								<TableCell>{item?.billing?.first_name}</TableCell>
-								<TableCell>{item?.billing?.phone}</TableCell>
-								<TableCell>{item?.total}руб</TableCell>
-								<TableCell>
-									<Dropdown>
-										<DropdownTrigger>
-											<Button
-													variant="bordered"
-											>
-												Посмотреть
-											</Button>
-										</DropdownTrigger>
-										<DropdownMenu aria-label="Static Actions" items={item?.line_items}>
-											{(line) => (
-													<DropdownItem key={line?.id}>{line?.name} - {line?.total}руб</DropdownItem>
-											)}
-										</DropdownMenu>
-									</Dropdown>
-								</TableCell>
-							</TableRow>
-					)}
-					
-				</TableBody>
-			</Table>
+			<div className="relative flex items-center justify-end">
+				<div>
+					<Button
+						className="text-sm font-normal text-default-600 bg-default-100"
+						variant="flat"
+						onClick={changeTableCard}
+					>
+						<TableIcon className="text-default-500" />
+						/
+						<CardIcon className="text-default-500" />
+					</Button>
+				</div>
+			</div>
+			{tableCard ?
+					<Table aria-label="Example table with dynamic content">
+						<TableHeader columns={columns}>
+							{(column) => <TableColumn key={column.key}>{column.label}</TableColumn>}
+						</TableHeader>
+						<TableBody items={orderList}>
+							{(item) => (
+									<TableRow key={item?.id}>
+										<TableCell>{item?.id}</TableCell>
+										<TableCell>{item?.status}</TableCell>
+										<TableCell>{item?.billing?.email}</TableCell>
+										<TableCell>{item?.billing?.first_name}</TableCell>
+										<TableCell>{item?.billing?.phone}</TableCell>
+										<TableCell>{item?.total}руб</TableCell>
+										<TableCell>
+											<Dropdown>
+												<DropdownTrigger>
+													<Button
+															variant="bordered"
+													>
+														Посмотреть
+													</Button>
+												</DropdownTrigger>
+												<DropdownMenu aria-label="Static Actions" items={item?.line_items}>
+													{(line) => (
+															<DropdownItem key={line?.id}>{line?.name} - {line?.total}руб</DropdownItem>
+													)}
+												</DropdownMenu>
+											</Dropdown>
+										</TableCell>
+									</TableRow>
+							)}
+
+						</TableBody>
+					</Table>
+			:
+					<div className="gap-2 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4">
+						{orderList.map((item) => (
+								<Card shadow="sm" key={item?.id} isPressable onPress={() => console.log("item pressed")}>
+									<CardBody className="overflow-visible p-4 gap-2">
+										<b>Заказ № {item?.id}</b>
+										<p className="text-default-500">Статус: {item?.status}</p>
+										<p className="text-default-500">{item?.billing?.email}</p>
+										<b>{item?.billing?.first_name}</b>
+										<p className="text-default-500">{item?.billing?.phone}</p>
+									</CardBody>
+									<CardFooter className="text-small justify-between">
+										<p className="text-default-500">Сумма: {item?.total}</p>
+										<Dropdown>
+											<DropdownTrigger>
+												<Button
+														variant="bordered"
+												>
+													Посмотреть
+												</Button>
+											</DropdownTrigger>
+											<DropdownMenu aria-label="Static Actions" items={item?.line_items}>
+												{(line) => (
+														<DropdownItem key={line?.id}>{line?.name} - {line?.total}руб</DropdownItem>
+												)}
+											</DropdownMenu>
+										</Dropdown>
+									</CardFooter>
+								</Card>
+						))}
+					</div>
+			}
+			
 			{loading ? <div className="flex justify-center items-center"><CircularProgress size="lg" aria-label="Loading..." /></div> : '' }
 
-			<div className="gap-2 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4">
-				{orderList.map((item) => (
-						<Card shadow="sm" key={item?.id} isPressable onPress={() => console.log("item pressed")}>
-							<CardBody className="overflow-visible p-4 gap-2">
-								<b>Заказ № {item?.id}</b>
-								<p className="text-default-500">Статус: {item?.status}</p>
-								<p className="text-default-500">{item?.billing?.email}</p>
-								<b>{item?.billing?.first_name}</b>
-								<p className="text-default-500">{item?.billing?.phone}</p>
-							</CardBody>
-							<CardFooter className="text-small justify-between">
-								<p className="text-default-500">Сумма: {item?.total}</p>
-								<Dropdown>
-									<DropdownTrigger>
-										<Button
-												variant="bordered"
-										>
-											Посмотреть
-										</Button>
-									</DropdownTrigger>
-									<DropdownMenu aria-label="Static Actions" items={item?.line_items}>
-										{(line) => (
-												<DropdownItem key={line?.id}>{line?.name} - {line?.total}руб</DropdownItem>
-										)}
-									</DropdownMenu>
-								</Dropdown>
-							</CardFooter>
-						</Card>
-				))}
-			</div>
+			
 		</section>
 	);
 }
